fdfd9b7132312a256e378792f92c67950ecb1de5
nieyuyao <nieyuyao0826@hotmail.com>
2025-02-06 17:51:00 +0800

    fix: incorrect origin pinyin

--- tests/engine.test.ts
+++ tests/engine.test.ts
@@ -4,5 +4,6 @@ import { getCandidates } from '../src/engine'
 describe('test engine output', () => {
   it('getCandidates', () => {
     expect(getCandidates('i')).toEqual([['i', 'I'], [1, 1]])
+    expect(getCandidates('nihaode')).toEqual([['你好'], [5]])
   })
 })
