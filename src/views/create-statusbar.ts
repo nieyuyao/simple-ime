a7eda8702217762b42f486f274c9d849663c66e7
nieyuyao <nieyuyao0826@hotmail.com>
2025-02-10 18:16:33 +0800

    feat: support drag and update ui

--- src/views/create-statusbar.ts
+++ src/views/create-statusbar.ts
@@ -1,10 +1,69 @@
 import banjiaoIcon from '../img/banjiao.png?inline'
-import chIcon from '../img/ch.png?inline'
+import dragIcon from '../img/drag.png?inline'
 import enIcon from '../img/en.png?inline'
+import chIcon from '../img/pin.png?inline'
 import quanjiaoIcon from '../img/quanjiao.png?inline'
 import semiEnIcon from '../img/semi-en.png?inline'
 import semiZhIcon from '../img/semi-zh.png?inline'
 import statusContentHtml from './status.html?raw'
+
+interface MousePosition { x: number, y: number }
+
+function dragStatusbar(draggedEel: HTMLImageElement, targetEl: HTMLElement) {
+  const lastPos: MousePosition = { x: 0, y: 0 }
+  const lastElBounding: MousePosition & { width: number, height: number } = { x: 0, y: 0, width: 0, height: 0 }
+  let enableDrag = false
+  const onmousedown = (e: MouseEvent) => {
+    if (e.target !== draggedEel) {
+      return
+    }
+    const rect = targetEl.getBoundingClientRect()
+    lastPos.x = e.clientX
+    lastPos.y = e.clientY
+    lastElBounding.x = rect.x
+    lastElBounding.y = rect.y
+    lastElBounding.width = rect.width
+    lastElBounding.height = rect.height
+    enableDrag = true
+  }
+  const onmousemove = (e: MouseEvent) => {
+    if (!enableDrag) {
+      return
+    }
+    const dx = e.clientX - lastPos.x
+    const dy = e.clientY - lastPos.y
+    let newLeft = lastElBounding.x + dx
+    let newTop = lastElBounding.y + dy
+    if (newLeft <= 0) {
+      newLeft = 0
+    }
+    else if (newLeft + lastElBounding.width >= window.innerWidth) {
+      newLeft = window.innerWidth - lastElBounding.width
+    }
+    if (newTop <= 0) {
+      newTop = 0
+    }
+    else if (newTop + lastElBounding.height >= window.innerHeight) {
+      newTop = window.innerHeight - lastElBounding.height
+    }
+    targetEl.style.left = `${newLeft}px`
+    targetEl.style.top = `${newTop}px`
+  }
+  const mouseleave = () => enableDrag = false
+  document.addEventListener('mousedown', onmousedown)
+  document.addEventListener('mousemove', onmousemove)
+  document.addEventListener('mouseup', mouseleave)
+  document.addEventListener('mouseleave', mouseleave)
+
+  return {
+    unbindEventHandlers() {
+      document.removeEventListener('mousedown', onmousedown)
+      document.removeEventListener('mousemove', onmousemove)
+      document.removeEventListener('mouseup', mouseleave)
+      document.removeEventListener('mouseleave', mouseleave)
+    },
+  }
+}
 
 export function createStatusBar(
   onMethodChange: (e: MouseEvent) => void,
@@ -17,10 +76,13 @@ export function createStatusBar(
     .replace('{pinyinIcon}', () => chIcon)
     .replace('{caseIcon}', () => quanjiaoIcon)
     .replace('{punctIcon}', () => semiZhIcon)
+    .replace('{dragIcon}', dragIcon)
   statusbarEl.innerHTML = contentHtml
   const methodIcons = [chIcon, enIcon]
   const shapeIcons = [quanjiaoIcon, banjiaoIcon]
   const punctIcons = [semiZhIcon, semiEnIcon]
+
+  let dragDisposer: ReturnType<typeof dragStatusbar> | null = null
 
   const icons = [methodIcons, shapeIcons, punctIcons]
   const currentIconIndexes = [0, 0, 0]
@@ -42,17 +104,20 @@ export function createStatusBar(
     })
   })
 
+  const dragEl = statusbarEl.querySelector<HTMLImageElement>('.sime-status-drag-icon')
+
+  if (dragEl) {
+    dragDisposer = dragStatusbar(dragEl, statusbarEl)
+  }
+
   document.body.append(statusbarEl)
 
   return {
     hide: () => statusbarEl.style.display = 'none',
     show: () => statusbarEl.style.display = 'flex',
-    dispose: () => statusbarEl.remove(),
-    updateMethodIcon: () => {
-      const methodIconEl = statusIconEls[0]
-      const idx = (currentIconIndexes[0] + 1) % 2
-      currentIconIndexes[0] = idx
-      methodIconEl.src = icons[0][idx]
+    dispose: () => {
+      statusbarEl.remove()
+      dragDisposer?.unbindEventHandlers()
     },
   }
 }
