a71d3bf3234a3b18b54aaaba8786f176ffafc23a
nieyuyao <nieyuyao0826@hotmail.com>
2025-02-11 12:09:09 +0800

    pref: reduce dict size

--- scripts/split-dict.ts
+++ scripts/split-dict.ts
@@ -6,7 +6,7 @@ interface Candidate { w: string, f: number }
 
 export function splitDict() {
   const __dirname = fileURLToPath(import.meta.url)
-  const dictString = fs.readFileSync(path.resolve(__dirname, '../../../dict/google-pinyin-dict.txt'), { encoding: 'utf-8' })
+  const dictString = fs.readFileSync(path.resolve(__dirname, '../../dict/google-pinyin-dict.txt'), { encoding: 'utf-8' })
   const dict = JSON.parse(dictString) as Record<string, Array<Candidate>>
   const coreDict: Record<string, Array<[string, number]>> = {}
   Object.keys(dict).forEach((pinyin) => {
@@ -20,7 +20,7 @@ export function splitDict() {
     })
   })
 
-  fs.writeFileSync(path.resolve(__dirname, '../../data/dict.txt'), JSON.stringify(coreDict))
+  fs.writeFileSync(path.resolve(__dirname, '../../src/data/dict.txt'), JSON.stringify(coreDict))
 }
 
 splitDict()
