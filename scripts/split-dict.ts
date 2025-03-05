2c9f73ec6d68bf14190f02c13ab4d5bb26c5c518
nieyuyao <nieyuyao0826@hotmail.com>
2025-03-05 22:36:32 +0800

    enhancement: adjust emoji freq

--- scripts/split-dict.ts
+++ scripts/split-dict.ts
@@ -1,15 +1,15 @@
-import type { Candidate } from '../src/types'
+import type { DictWord } from '../src/engine'
 import fs from 'node:fs'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 import { Trie } from 'dawg-lookup'
-import { compress } from './compress-dict.js'
+import { compress, deserializeDict } from './compress-dict.js'
 
 function splitDict() {
   const __dirname = fileURLToPath(import.meta.url)
   const baseDictContent = fs.readFileSync(path.resolve(__dirname, '../../dict/google-pinyin-dict.txt'), { encoding: 'utf-8' })
-  const baseDict = JSON.parse(baseDictContent) as Record<string, Array<Candidate>>
-  const dict: Record<string, Array<[string, number]>> = {}
+  const baseDict = deserializeDict(baseDictContent)
+  const dict: Record<string, Array<DictWord>> = {}
   const pinyinList: string[] = []
   Object.keys(baseDict).forEach((pinyin) => {
     baseDict[pinyin].forEach((candidate) => {
@@ -17,7 +17,7 @@ function splitDict() {
         if (!dict[pinyin]) {
           dict[pinyin] = []
         }
-        dict[pinyin].push([candidate.w, candidate.f])
+        dict[pinyin].push({ ...candidate })
         pinyinList.push(pinyin)
       }
     })
@@ -26,7 +26,7 @@ function splitDict() {
     fs.mkdirSync(path.resolve(__dirname, '../../temp'))
   }
   const trie = new Trie(pinyinList.join(' '))
-  fs.writeFileSync(path.resolve(__dirname, '../../temp/dict.txt'), JSON.stringify(compress(JSON.stringify(dict))))
+  fs.writeFileSync(path.resolve(__dirname, '../../temp/dict.txt'), JSON.stringify(compress(dict)))
   fs.writeFileSync(path.resolve(__dirname, '../../temp/packed-trie.txt'), trie.pack())
 }
 
